240c6b99e3144e9115a68d2d5d9aec3d5e79ddb1
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-08-02 01:32:44 +0600

    admin & landlord user profile is updated

--- app/(dashboardRoute)/admin-dashboard/profile/page.tsx
+++ app/(dashboardRoute)/admin-dashboard/profile/page.tsx
@@ -1,11 +1,34 @@
+import ProfileForm from '../../tenant-dashboard/profile/_components/profile-form';
+import { cookies } from 'next/headers';
 
+const getMyProfile = async () => {
+    const cookieStore = await cookies();
 
-const AdminProfile = () => {
-  return (
-    <div>
-      Admin Profile Page
-    </div>
-  )
-}
+    const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/me`, {
+        headers: {
+            Cookie: cookieStore.toString(),
+        },
 
-export default AdminProfile
+        cache: 'no-store',
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch profile');
+    }
+
+    const result = await res.json();
+
+    return result.data.profile;
+};
+
+const TenantProfilePage = async () => {
+    const profile = await getMyProfile();
+
+    return (
+        <div className="container mx-auto py-8">
+            <ProfileForm profile={profile} />
+        </div>
+    );
+};
+
+export default TenantProfilePage;
